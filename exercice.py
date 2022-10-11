8c0eecef3d28d1ad7208a1f7a3a4197351c6d405
nayarramo <nayarramo@outlook.com>
2022-10-11 12:31:17 -0400

    fin

--- exercice.py
+++ exercice.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# from curses.ascii import isalnum
 import math
 
 def get_num_letters(text):
-	return 0
+	num_letter=0
+	for letter in text:
+		num_letter += letter.isalnum()
+	return num_letter
 
 def get_word_length_histogram(text):
-	return [0]
+	list_words, len_words, histogram = text.split(" "), [], [0]
+	for word in list_words:
+		len_words += [get_num_letters(word)]
+	for i in range(max(len_words)):
+		histogram += [0]
+	for length in len_words:
+		histogram[length] += 1
+	return histogram
 
 def format_histogram(histogram):
-	ROW_CHAR = "*"
-
-	return ""
+	ROW_CHAR, position, form_hist = "*", 0, ""
+	for i in range(1, len(histogram)):
+		position+=1
+		number = ROW_CHAR * histogram[i]
+		form_hist += "\n" + f"{position:2} {number}"
+	return form_hist
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
-
-	return ""
+	result = ""
+	for i in range(1, max(histogram[1:])+1):
+		for value in histogram[1:]:
+			if value >= max(histogram[1:])+1-i:
+				result+= BLOCK_CHAR
+			else:
+				result+= " "
+		result+= "\n"
+	result+= LINE_CHAR*len(histogram)
+	return result
 
 
 if __name__ == "__main__":
